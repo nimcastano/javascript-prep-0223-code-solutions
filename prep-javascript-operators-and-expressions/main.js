e9e86338ba7e3f834f7a64873e320044d9b8e569
Nimrod Castano <nimrodcastano@gmail.com>
2023-03-01 05:00:08 +0000

    Completed prep-javascript-operators-and-expressions git status

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const number1 = 5;
+const number2 = 3;
+const product = number1 * number2;
+console.log('value of product: ' + product);
+console.log('typeof product: ' + typeof product);
+
+const charge = 12;
+const payment = 9;
+const amountRemaining = charge - payment;
+console.log('value of product: ' + amountRemaining);
+console.log('typeof product: ' + typeof amountRemaining);
+
+const tests = 32;
+const assignments = 22;
+const final = 1;
+const grade = (tests + assignments + final) / 3;
+console.log('value of product: ' + grade);
+console.log('typeof product: ' + typeof grade);
+
+const firstName = 'Nimrod';
+const lastName = 'Castano';
+const fullName = firstName + ' ' + lastName;
+console.log('value of product: ' + fullName);
+console.log('typeof product: ' + typeof fullName);
+
+const pH = 3;
+const isAcidic = pH < 7;
+console.log('value of product: ' + isAcidic);
+console.log('typeof product: ' + typeof isAcidic);
+
+const numberOfSoldiers = 440;
+const isSparta = numberOfSoldiers === 300;
+console.log('value of product: ' + isSparta);
+console.log('typeof product: ' + typeof isSparta);
+
+let nickname = fullName;
+nickname += ' is the GOAT';
+console.log('value of product: ' + nickname);
+console.log('typeof product: ' + typeof nickname);
---
+++ prep-javascript-operators-and-expressions/prep-javascript-operators-and-expressions-quiz-notes.md
@@ -0,0 +1,43 @@
+# prep-javascript-operators-and-expressions-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What data type is returned by an arithmetic operation?
+Arithmetic operators return a single numerical value.
+- What is string concatenation?
+String concatenation is essentially the manipulation of strings, more specifically the action of combining two strings.
+- What purpose(s) does the `+` plus operator serve in JavaScript?
+The '+' operator performs the addition of numbers. It is also used for string concatenation.
+- What data type is returned by comparing two values (`<`, `>`, `===`, etc)?
+The data type returned using the aforementioned operators is a boolean.
+- What does the `+=` "plus-equals" operator do?
+The '+=" operator adds a value, the number on the right hand side, to the variable on the left hand side.
+
+
+## Notes
+
+All student notes should be written here.
+
+
+How to write `Code Examples` in markdown
+
+for JS:
+```javascript
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
