6ec63577ff383c2e9176163b08b7c1c2a2dc85c2
Nimrod Castano <nimrodcastano@gmail.com>
2023-03-08 04:15:24 +0000

    Completed prep-array-challenge.

---
+++ prep-array-challenge/src/compact.js
@@ -0,0 +1,10 @@
+/* exported compact */
+const compact = arr => {
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
---
+++ prep-array-challenge/src/last.js
@@ -0,0 +1,4 @@
+/* exported last */
+const last = arr => {
+  return arr[arr.length - 1];
+};
